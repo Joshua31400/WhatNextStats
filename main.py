ddf6498c169a2b6266e763a7e0f903fbd6c50838
bjoshua <joshua.budgen@ynov.com>
2026-03-09 17:38:46 +0100

    feat(heat_map.py): How notes evolv in time - I associate name with ID and after assign it. After I build the pivot for the layout on the heatmap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from data_clean import DataCleaner
+from heat_map import HeatmapGenerator
 
 API_KEY  = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTAzYmNiZTYwZTZlMWYwYTk2MzczYWEzZjFkMDQ3YiIsIm5iZiI6MTc3MjQ0NTExMy40OTQwMDAyLCJzdWIiOiI2OWE1NWRiOThlODAzN2ExNWJjMTdkYTIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.W1Yi9LObmdHr90359Xkzj37iHavmsU8FeEMHADzzEEg"
 
@@ -25,3 +26,6 @@ if __name__ == "__main__":
 
     print(f" Cleaned movies: {len(df_clean)}")
     print(df_clean.to_string())
+
+    heatmap = HeatmapGenerator(df_clean)
+    heatmap.generate()
